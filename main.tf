932a9e11aa504723d05974cff1763d3ac061f5d8
daniloamaral01 <85200994+daniloamaral01@users.noreply.github.com>
2021-10-14 23:50:56 -0300

    Exercício2

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "slacko-app" {
+data "aws_ami" "slacko-app"{
     most_recent = true
     owners = ["amazon"]
 
@@ -13,169 +13,138 @@ data "aws_ami" "slacko-app" {
     }
 }
 
-data "aws_vpc" "my-vpc" {
-  filter {
-    name = "tag:Name"
-    values = ["my-vpc"]
-  }
+data "aws_subnet" "subnet_public" {
+    cidr_block = var.subnet_cidr
 }
 
-data "aws_subnet" "subnet_public"{
-    cidr_block = "10.0.102.0/24"
-} 
-
+# SSH Key
 resource "aws_key_pair" "slacko-sshkey" {
-    key_name = "slacko-app-key"
-    public_key = "*********"
+    key_name = format("%s-app-key", var.app_name)
+    public_key = var.ssh_key
+  
 }
 
 resource "aws_instance" "slacko-app" {
     ami = data.aws_ami.slacko-app.id
-    instance_type = "t2.micro"
+    instance_type = var.app_instance
     subnet_id = data.aws_subnet.subnet_public.id
     associate_public_ip_address = true
-
-    tags = {
-        Name = "Slacko-app"
-    }
+    tags = merge(var.app_tags,
+            {
+            "Name" = format("%s-app", var.app_name)
+            },)
     key_name = aws_key_pair.slacko-sshkey.id
-    user_data = file("ec2.sh") 
+    # bootstrap file
+    user_data = file("${path.module}/files/ec2.sh")
+
 }
 
 resource "aws_instance" "mongodb" {
     ami = data.aws_ami.slacko-app.id
-    instance_type = "t2.small"
+    instance_type = var.db_instance
     subnet_id = data.aws_subnet.subnet_public.id
- 
-    tags = {
-        Name = "MongoDb"
-    }
 
+    tags = merge(var.app_tags,{
+      "Name" = format("%s-mongodb", var.app_name)
+    },)
     key_name = aws_key_pair.slacko-sshkey.id
-    user_data = file("mongodb.sh") 
+    user_data = file("${path.module}/files/mongodb.sh")
 }
 
-resource  "aws_security_group" "allow_slacko"{
-    name = "allow_ssh_http"
-    description = "Allow ssh and https port"
-    vpc_id = data.aws_vpc.my-vpc.id
-    
-    ingress = [
-        {
-            description = "Allow SSH"
-            from_port = 22
-            to_port = 22
-            protocol = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = ["::/0"]
-            prefix_list_ids = null
-            security_groups = null
-            self = null
-        },
-        {
-            description = "Allow http"
-            from_port = 80
-            to_port = 80
-            protocol = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = ["::/0"]
-            prefix_list_ids = null
-            security_groups = null
-            self = null
-        }
-    ]
-    egress = [
-        {
-            description = "Allow all"
-            from_port = 0
-            to_port = 0
-            protocol = "-1"
-            cidr_blocks = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = ["::/0"]
-            prefix_list_ids = null
-            security_groups = null
-            self = null
-        }      
-    ]
-    tags = {
-        Name = "Allow_ssh_http"
-    }
-
+resource "aws_security_group" "allow-slacko" {
+    name = format("%s-sg-app", var.app_name)
+    description = "allow ssh and http port"
+    vpc_id = var.vpc_id
+    tags = merge(var.app_tags,{
+        "Name" = format("%s-sg-app", var.app_name)
+    })
 }
 
-resource "aws_security_group" "allow-mongodb"{
-    name = "allow_mongodb"
-    description = "Allow MongoDb"
-    vpc_id = data.aws_vpc.my-vpc.id
+resource "aws_security_group_rule" "allow-slacko-ingress-http" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
-    ingress = [
-        {
-            description = "Allow MOngoDB"
-            from_port = 27017
-            to_port = 27017
-            protocol = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = ["::/0"]
-            prefix_list_ids = null
-            security_groups = null
-            self = null
-
-        }
-    ]
-    egress = [
-        {
-            description = "Allow all"
-            from_port = 0
-            to_port = 0
-            protocol = "all"
-            cidr_blocks = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = ["::/0"]
-            prefix_list_ids = null
-            security_groups = null
-            self = null
-
-        }
-
-    ]
-
-    tags = {
-        Name = "allow_mongodb"
-    }
-
-
+    security_group_id = aws_security_group.allow-slacko.id
 }
-## associando o sg
-resource "aws_network_interface_sg_attachment" "mongo-sg" {
+
+resource "aws_security_group_rule" "allow-slacko-ingress-ssh" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.allow-slacko.id
+}
+
+resource "aws_security_group_rule" "allow-slacko-egress-all" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.allow-slacko.id
+}
+
+
+
+resource "aws_security_group" "allow-mongodb" {
+    name = format("%s-sg-db", var.app_name)
+    description = "allow nmongodb"
+    vpc_id = var.vpc_id
+    tags = merge(var.app_tags,{
+        "Name" = format("%s-sg-db", var.app_name)
+    })
+}
+
+resource "aws_security_group_rule" "allow-mongo-ingress-mongo" {
+    type = "ingress"
+    from_port = 27017
+    to_port = 27017
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.allow-mongodb.id
+  
+}
+
+resource "aws_security_group_rule" "allow-mongo-egress-all" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.allow-mongodb.id
+  
+}
+
+resource "aws_network_interface_sg_attachment" "mongodb-sg" {
     security_group_id = aws_security_group.allow-mongodb.id
     network_interface_id = aws_instance.mongodb.primary_network_interface_id
 }
 
-## associando o SG
-resource "aws_network_interface_sg_attachment" "slacko-sg" {
-    security_group_id = aws_security_group.allow_slacko.id
+resource "aws_network_interface_sg_attachment" "slacko" {
+    security_group_id = aws_security_group.allow-slacko.id
     network_interface_id = aws_instance.slacko-app.primary_network_interface_id
 }
 
-resource "aws_route53_zone" "slack_zone" {
-    name = "iaac0506.com.br"
-    vpc{
-        vpc_id = data.aws_vpc.my-vpc.id
+resource "aws_route53_zone" "slacko_zone" {
+    name = format("%s-route53-zone-app", var.app_name)
+    vpc {
+        vpc_id = var.vpc_id
     }
-    
+    tags = var.app_tags
 }
 
 resource "aws_route53_record" "mongodb" {
-    zone_id = aws_route53_zone.slack_zone.id
-    name = "mongodb.iaac0506.com.br"
+    zone_id = aws_route53_zone.slacko_zone.id
+    name = format("%s-route53-rec-db", var.app_name)
     type = "A"
-    ttl = "3600"
+    ttl = "300"
     records = [aws_instance.mongodb.private_ip]
-}
-
-output "slacko-mongodb" {
-    value = aws_instance.mongodb.private_ip
-}
-
-output "slacko-app" {
-    value = aws_instance.slacko-app.public_ip
 }
